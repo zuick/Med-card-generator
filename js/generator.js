e522d37bc43d1da9d8cf0b9a3b78c5865f5798ec
zuick <izuick@gmail.com>
2012-08-11 03:19:52 +0500

    med card 07

--- js/generator.js
+++ js/generator.js
@@ -13,10 +13,10 @@ function initialization(){
 	$('.param-box table div.checkboxes').each(function(){
 		add_custom_input( this );
 	});
-	$('input.custom-input').live( 'click', function(){
-		generate();
-	});
-	
+	$('input').live( 'change', function(){ generate(); });
+	$('select').live( 'change', function(){ generate(); });
+	$('.datepicker .today').live( 'click', function(){ generate(); });
+	$('.datepicker .date').live( 'click', function(){ generate(); });
 	// добавить выбор даты
 	$('#date').simpleDatepicker(); 
 }
@@ -39,33 +39,33 @@ function get_custom_input_val( field_id ){
 }
 
 // Функции заполнения превью справки
-function fill_from_checkboxes( field_id, field_name ){
+function fill_from_checkboxes( field_id, field_name, separator ){
 	var p_field_id = '#' + field_id + '-p';
 	
 	if( custom_input_empty( field_id ) ){
 		var is_first = true;
-		$(p_field_id).text( field_name + ': ');
+		$(p_field_id).html( '<pre>' + field_name + separator + '</pre>' );
 		$('#' + field_id + ' div input').each( function() {
 			if( this.checked ){
 				if( is_first ){
-					$(p_field_id).text( $(p_field_id).text() + $(this).val() );
+					$(p_field_id).html( $(p_field_id).html() + $(this).val() );
 					is_first = false;
 				}else{
-					$(p_field_id).text( $(p_field_id).text() + ', '+ $(this).val() );
+					$(p_field_id).html( $(p_field_id).html() + ', '+ $(this).val() );
 				}			
 			}
 		});	
 	}else{
-		$(p_field_id).text( field_name + ': ' + get_custom_input_val( field_id ));
+		$(p_field_id).html( '<pre>' + field_name + separator + '</pre>' + get_custom_input_val( field_id ));
 	}
 	
 
 }
 
-function fill_from_select( field_id, field_name ){
+function fill_from_select( field_id, field_name, separator){
 	var addition = '';
 	addition = ( custom_input_empty( field_id ) ) ? $('#' + field_id).val() : get_custom_input_val( field_id );		
-	$('#' + field_id + '-p').text( field_name + ': ' + addition);
+	$('#' + field_id + '-p').html( '<pre>' + field_name + separator + '</pre>' + addition);
 }
 
 function fill_from_double_select( id_main, id_second, field_name){
@@ -75,8 +75,8 @@ function fill_from_double_select( id_main, id_second, field_name){
 	$('#' + id_main + '-p').text( field_name + ': ' + addition );
 }
 
-function fill_from_input( field_id, field_name ){
-	fill_from_select( field_id, field_name );
+function fill_from_input( field_id, field_name, separator){
+	fill_from_select( field_id, field_name, separator );
 }
 
 function fill_from_radiobutton( field_id, field_name ){
@@ -86,26 +86,26 @@ function fill_from_radiobutton( field_id, field_name ){
 // генерация текста справки
 function generate(){
 	fill_from_radiobutton( 'type', '' );
-	fill_from_input( 'date', 'Дата');
-	fill_from_input( 'temp', 'Темп');
-	fill_from_input( 'chd', 'ЧД');
-	fill_from_input( 'chss', 'ЧСС');
-	fill_from_select( 'name', 'ФИО');
+	fill_from_input( 'date', '', '');
+	fill_from_input( 'temp', 'Темп', ':');
+	fill_from_input( 'chd', 'ЧД', ':');
+	fill_from_input( 'chss', 'ЧСС', ':');
+	fill_from_select( 'name', '', '');
 	fill_from_double_select( 'state', 'state-level', 'Состояние');
-	fill_from_select( 'skin', 'Кожа');
-	fill_from_select( 'dyha', 'Дыхание');
-	fill_from_select( 'serd', 'Сердечные тоны');
-	fill_from_select( 'jivo', 'Живот');
-	fill_from_select( 'stul', 'Стул');
-	fill_from_select( 'moch', 'Мочеиспускание');
-	fill_from_checkboxes('zev', 'Зев');
-	fill_from_checkboxes('kash', 'Кашель');
-	fill_from_checkboxes('limf', 'Лимфотические узлы');
-	fill_from_checkboxes('nosd', 'Носовое дыхание');	
+	fill_from_select( 'skin', 'Кожа', ':');
+	fill_from_select( 'dyha', 'Дыхание', ':');
+	fill_from_select( 'serd', 'Сердечные тоны', ':');
+	fill_from_select( 'jivo', 'Живот', ':');
+	fill_from_select( 'stul', 'Стул', ':');
+	fill_from_select( 'moch', 'Мочеиспускание', ':');
+	fill_from_checkboxes('zev', 'Зев', ':');
+	fill_from_checkboxes('kash', 'Кашель', ':');
+	fill_from_checkboxes('limf', 'Лимфотические узлы', ':');
+	fill_from_checkboxes('nosd', 'Носовое дыхание', ':');	
 
 	if($('input[name=zdorov]').prop('checked'))
 		$('#yavka-p').text('');
 	else
-		fill_from_input( 'yavka', 'Явка');	
+		fill_from_input( 'yavka', 'Явка', ':');	
 
 }
